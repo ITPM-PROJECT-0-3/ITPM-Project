bab7f5f66b14b814cb13ed39f7ff9fe08bb193ff
Hashini Rathnayake <110319538+IT21802508@users.noreply.github.com>
2024-03-16 06:23:31 +0530

    Add staff modules

---
+++ BACKEND/controller/staffController.js
@@ -0,0 +1,81 @@
+const StaffUser = require("../model/StaffModel");
+
+const createStaffUser = async (req, res) => {
+  try {
+    const { firstName, lastName, department, email, phone } = req.body;
+    const staffUser = new StaffUser({
+      firstName,
+      lastName,
+      department,
+      email,
+      phone,
+    });
+    const savedStaffUser = await staffUser.save();
+    res.status(201).json(savedStaffUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getAllStaffUsers = async (req, res) => {
+  try {
+    const staffUsers = await StaffUser.find();
+    res.json(staffUsers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getStaffUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const staffUser = await StaffUser.findById(id);
+    if (!staffUser) {
+      return res.status(404).json({ error: "Staff user not found" });
+    }
+    res.json(staffUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const updateStaffUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedStaffUser = await StaffUser.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedStaffUser) {
+      return res.status(404).json({ error: "Staff user not found" });
+    }
+    res.json(updatedStaffUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const deleteStaffUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedStaffUser = await StaffUser.findByIdAndDelete(id);
+    if (!deletedStaffUser) {
+      return res.status(404).json({ error: "Staff user not found" });
+    }
+    res.json({ message: "Staff user deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  createStaffUser,
+  getAllStaffUsers,
+  getStaffUserById,
+  updateStaffUserById,
+  deleteStaffUserById,
+};
---
+++ BACKEND/model/StaffModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const staffSchema = new mongoose.Schema(
+  {
+    
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    
+    department: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    Phone: {
+      type: String,
+    },
+    
+  },
+ 
+);
+
+const staffUser = mongoose.model("staffUser", staffSchema);
+
+module.exports = staffUser;
